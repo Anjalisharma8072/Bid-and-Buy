c7f9e3666560bde04502fc7e03fcbdf46be18336
Anjali Sharma <anjaliiisharma007@gmail.com>
2024-08-15 23:25:48 +0530

    Update config.js

--- auction-backend/config.js
+++ auction-backend/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   CLIENT_ID:
-    "635005369096-tgb0o3mqc9diq8tatri4epqdmdnilimk.apps.googleusercontent.com",
-  CLIENT_SECRET: "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",
-  REDIRECT_URI: "https://developers.google.com/oauthplayground",
+    "",
+  CLIENT_SECRET: "",
+  REDIRECT_URI: "",
   REFRESH_TOKEN:
-    "1//042gWeN3IDD2ZCgYIARAAGAQSNwF-L9Irf7zzP7p2NOe6B9BOfq0AsPRzN7q6Zwpw7x3XGoGktJjmsgWl_hn5BUsh6UqHQrS08GM",
+    "",
 };
