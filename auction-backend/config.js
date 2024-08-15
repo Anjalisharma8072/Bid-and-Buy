d5608560ca58252ad0a1a28c84407dc230fd871e
Anjalisharma8072 <anjaliiisharma007@gmail.com>
2024-08-15 23:20:39 +0530

    changes

--- auction-backend/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  CLIENT_ID:
-    "635005369096-tgb0o3mqc9diq8tatri4epqdmdnilimk.apps.googleusercontent.com",
-  CLIENT_SECRET: "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",
-  REDIRECT_URI: "https://developers.google.com/oauthplayground",
-  REFRESH_TOKEN:
-    "1//042gWeN3IDD2ZCgYIARAAGAQSNwF-L9Irf7zzP7p2NOe6B9BOfq0AsPRzN7q6Zwpw7x3XGoGktJjmsgWl_hn5BUsh6UqHQrS08GM",
-};
